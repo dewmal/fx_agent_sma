49d8f8fbbc02ce29e8848f590dac73bbd475bb53
dewmal <dewmalnilanka@gmail.com>
2019-07-06 18:34:02 +0530

    Technical Agent Remove Defects

--- agents/technical_agent.py
+++ agents/technical_agent.py
@@ -23,29 +23,7 @@ class FxTechnicalBehaviour(CyclicBehaviour):
             # print(tick_id)
             fx_tick = await fx_db.get_fx_tick(tick_id)
 
-            self.window_que.put(fx_tick)
-            # print("put done")
-
-            if self.window_que.qsize() == 5:
-                sma_value = np.random.uniform(0, 1)  # sma([x.ask for x in list(self.window_que.queue)], 5)
-                ema_value = np.random.uniform(0, 1)  # ema([x.ask for x in list(self.window_que.queue)], 5)
-                wma_value = np.random.uniform(0, 1)  # wma([x.ask for x in list(self.window_que.queue)], 5)
-                rsi_value = np.random.uniform(0, 1)  # rsi([x.ask for x in list(self.window_que.queue)], 5)
-
-                ta_value = ta_rule_ge_value(sma_value, ema_value, wma_value, rsi_value)
-
-                # direction = ta_rule_ge_direction([x.ask for x in list(self.window_que.queue)])
-
-                msg = Message(to=get_xmpp_username(username=users['decision']['username']))
-                msg.set_metadata("stream", "ta_result")
-                msg.set_metadata("symbol", fx_tick.pair)
-                msg.set_metadata("signal", "BUY")
-
-                msg.set_metadata("ta_value", f"{ta_value}")
-
-                await self.send(msg)
-
-                self.window_que.get()
+            print(fx_tick)
 
         await asyncio.sleep(delay=1)
 
