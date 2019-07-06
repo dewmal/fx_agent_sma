f0c716c89c64d703a9adb398270b6f24e2c57a02
dewmal <dewmalnilanka@gmail.com>
2019-07-06 18:28:35 +0530

    Organize agents starting

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
 import time
 
 from agents.coordinator_agent import CoordinatorAgent
+from agents.decision_agent import DecisionAgent
 from agents.fundamental_agent import FundamentalAnalysingAgent
 from agents.technical_agent import TechnicalAnalysingAgent
 from agents.trading_stream_agent import TradingStreamAgent
 from settings import *
 
-print(users['coordinator']['username'])
-
 if __name__ == "__main__":
 
-    fa_di_agent = CoordinatorAgent(get_xmpp_username(users['decision']['username']),
-                                   users['decision']['password'])
-    fa_di_agent.start()
-    fa_di_agent.web.start(hostname="127.0.0.1", port=users['decision']['port'])
+    agents = []
 
-    fa_co_agent = CoordinatorAgent(get_xmpp_username(users['coordinator']['username']),
-                                   users['coordinator']['password'])
-    fa_co_agent.start()
-    fa_co_agent.web.start(hostname="127.0.0.1", port=users['coordinator']['port'])
+    fa_di_agent = DecisionAgent(get_xmpp_username(
+        users['decision']['username']),
+        users['decision']['password'])
+    agents.append([fa_di_agent, users['decision']['port']])
 
-    fa_ta_agent = TechnicalAnalysingAgent(get_xmpp_username(users['technical']['username']),
-                                          users['technical']['password'])
-    fa_ta_agent.start()
-    fa_ta_agent.web.start(hostname="127.0.0.1", port=users['technical']['port'])
+    fa_co_agent = CoordinatorAgent(get_xmpp_username(
+        users['coordinator']['username']),
+        users['coordinator']['password'])
+    agents.append([fa_co_agent, users['coordinator']['port']])
 
-    fa_agent = FundamentalAnalysingAgent(get_xmpp_username(users['fundamental']['username']),
-                                         users['fundamental']['password'], news_read_frequency=60 * 60 * 2)
-    fa_agent.start()
-    fa_agent.web.start(hostname="127.0.0.1", port=users['fundamental']['port'])
+    fa_ta_agent = TechnicalAnalysingAgent(get_xmpp_username(
+        users['technical']['username']),
+        users['technical']['password'])
+    agents.append([fa_ta_agent, users['technical']['port']])
 
-    fa_trading_stream_agent = TradingStreamAgent(get_xmpp_username(users['stream_agent']['username']),
-                                                 users['stream_agent']['password'], stock_indexes=stock_indexes)
+    fa_agent = FundamentalAnalysingAgent(get_xmpp_username(
+        users['fundamental']['username']),
+        users['fundamental']['password'],
+        news_read_frequency=60 * 60 * 2)
+    agents.append([fa_agent, users['fundamental']['port']])
 
-    fa_trading_stream_agent.start()
-    fa_trading_stream_agent.web.start(hostname="127.0.0.1", port=users['stream_agent']['port'])
+    fa_trading_stream_agent = TradingStreamAgent(get_xmpp_username(
+        users['stream_agent']['username']),
+        users['stream_agent']['password'],
+        stock_indexes=stock_indexes)
+
+    agents.append([fa_trading_stream_agent, users['stream_agent']['port']])
+
+    for agent, port in agents:
+        agent.start()
+        agent.web.start(hostname="127.0.0.1", port=port)
 
     print("Wait until user interrupts with ctrl+C")
     while True:
